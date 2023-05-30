bb1dd0efc4263cbaa114abc3793ec66e47566703
WangJ <1170180928@qq.com>
2023-05-30 17:29:53 +0800

    整理代碼

--- src/main/java/com/OnegJack/Music/config/Cors.java
+++ src/main/java/com/OnegJack/Music/config/Cors.java
@@ -1,20 +1,20 @@
 package com.OnegJack.Music.config;
 
-import jakarta.servlet.annotation.WebFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.logging.Filter;
 @Configuration
 public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
-        corsRegistry.addMapping("/**").allowedOrigins("*").allowedMethods("POST","GET","DELETE","PUT").allowedHeaders("*");
+        corsRegistry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("POST","GET","DELETE","PUT")
+                .allowedHeaders("*");
     }
-    @Override
+/*    @Override
     public void addViewControllers(ViewControllerRegistry viewControllerRegistry){
-        viewControllerRegistry.addViewController("/").setViewName("/index.html");
-    }
+        viewControllerRegistry.addViewController("/")
+                .setViewName("/view/");
+    }*/
 }
--- src/main/java/com/OnegJack/Music/service/UserService.java
+++ src/main/java/com/OnegJack/Music/service/UserService.java
@@ -1,6 +1,5 @@
 package com.OnegJack.Music.service;
 
-import com.OnegJack.Music.domain.User;
 import com.OnegJack.Music.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +9,9 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+/*    Optional<User> find(String id){
+        if (userRepository.existsById(id)){
+            return  userRepository.findById(id);
+        }
+    }*/
 }
